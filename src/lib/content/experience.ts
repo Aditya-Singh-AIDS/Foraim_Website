bec8a5c1ca5c0e1c151db2a103fb9b8acfb2fb27
Aditya Singh <155745808+Aditya-Singh-AIDS@users.noreply.github.com>
2024-04-17 22:25:29 +0530

    Update experience.ts

--- src/lib/content/experience.ts
+++ src/lib/content/experience.ts
@@ -12,6 +12,8 @@ export const experienceSection: ExperienceSectionType = {
         upto: '',
         tasks: [
         'Quote: ',
+        'Dear Committee Members,',
+        'I wanted to take a moment to express my sincere appreciation for all the hard work and dedication you have shown in preparing for our upcoming Mega Event, "Sahaswara." Your commitment to excellence and your willingness to go above and beyond have not gone unnoticed.',
         ],
     },
     {
@@ -22,7 +24,7 @@ export const experienceSection: ExperienceSectionType = {
         started: '',
         upto: '',
         tasks: [
-        'Quote: ',
+        'Quote: To our Dedicated Committee Members, your hard work and commitment are the driving force behind the success of SAHASWARA Thank you for your unwavering dedication and passion!',
         ],
     },
     {
@@ -33,7 +35,29 @@ export const experienceSection: ExperienceSectionType = {
         started: '',
         upto: '',
         tasks: [
-        'Quote: ',
+        'Quote: Each of you plays a vital role in shaping this fest. Your ideas, energy, and enthusiasm empower us to reach new heights. Let\'s support each other and make this event truly remarkable!',
+        ],
+    },
+    {
+        img: '/VP.jpg',
+        company: 'Secretary',
+        companyUrl: '',
+        role: 'Sanika Zodape',
+        started: '',
+        upto: '',
+        tasks: [
+        'Quote: This fest is not just about organizing an event; it\'s about overcoming challenges, pushing boundaries, and achieving greatness. Let\'s embrace this journey with determination and pride.',
+        ],
+    },
+    {
+        img: '/VP.jpg',
+        company: 'Joint Secretary',
+        companyUrl: '',
+        role: 'Sudhanshu kodke',
+        started: '',
+        upto: '',
+        tasks: [
+        'Quote: Our committee represents the diversity and talent of our college. Let\'s celebrate our differences and work together to showcase the best of our college through this fest.',
         ],
     },
     ],
