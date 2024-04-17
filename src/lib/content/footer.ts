6e1a02825c79bdd9b6f019716b9f80c8785e3d7b
Aditya Singh <155745808+Aditya-Singh-AIDS@users.noreply.github.com>
2024-04-17 18:02:40 +0530

    Update footer.ts

--- src/lib/content/footer.ts
+++ src/lib/content/footer.ts
@@ -2,5 +2,5 @@ import { FooterSectionType } from '@/lib/types/sections';
 
 export const footerSection: FooterSectionType = {
     title: 'Design & Built by Aditya Singh and Vedant Ekale',
-    link: 'https://github.com/vatsalsinghkv/portfolio-website',
+    link: 'https://github.com/Aditya-Singh-AIDS',
 };
